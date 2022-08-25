d595698074aa8f07ce32369e4224a62637373c76
Thomas Ricouard <ricouard77@gmail.com>
2022-08-25 10:57:09 +0200

    Set font to Menlo

--- Pod/Classes/Theme.swift
+++ Pod/Classes/Theme.swift
@@ -52,7 +52,7 @@ open class Theme {
     init(themeString: String)
     {
         theme = themeString
-        setCodeFont(RPFont(name: "Courier", size: 14)!)
+        setCodeFont(RPFont(name: "Menlo", size: 12)!)
         strippedTheme = stripTheme(themeString)
         lightTheme = strippedThemeToString(strippedTheme)
         themeDict = strippedThemeToTheme(strippedTheme)
